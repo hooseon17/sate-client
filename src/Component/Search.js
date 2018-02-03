4d944903fe1dd140e7cb06fa003ae6fbfd229825
Hamza Arab <hamzaarab@cmail.carleton.ca>
2018-02-03 13:30:33 -0500

    Testing

--- src/Component/Search.js
+++ src/Component/Search.js
@@ -10,7 +10,7 @@ export default class Search extends React.Component {
                     <div class="bg-image"></div>
                     <div class="content-wrap">
                         <h1>Sate</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates dolore delectus quia iure possimus laborum expedita
+                        <p>Lorem ipsum dolorss sit amet, consectetur adipisicing elit. Voluptates dolore delectus quia iure possimus laborum expedita
                             ex inventore ea aliquid.</p>
                         <a href="#showcase" class="btn">Register</a>
                     </div>
