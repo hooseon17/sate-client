7f02086d7909495a86c007b297c6c4cc893bb27c
Unknown <hoo.yim@gmail.com>
2018-02-03 13:17:15 -0500

    Add Search Component

---
+++ src/Component/Search.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Search.css';
+
+export default class Search extends React.Component {
+
+    render(){
+        return (
+            <div className="Search">
+                <div id="showcase" class="grid">
+                    <div class="bg-image"></div>
+                    <div class="content-wrap">
+                        <h1>Sate</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates dolore delectus quia iure possimus laborum expedita
+                            ex inventore ea aliquid.</p>
+                        <a href="#showcase" class="btn">Register</a>
+                    </div>
+                </div>
+
+            </div>
+            
+        );
+    }
+
+}
