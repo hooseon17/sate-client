5936f20c66e4bf836f73ce9a69240eec8c9d05fd
Hamza Arab <hamzaarab@cmail.carleton.ca>
2018-02-03 14:36:10 -0500

    Edited text to layout for "SATE"

--- src/Component/Search.js
+++ src/Component/Search.js
@@ -10,8 +10,7 @@ export default class Search extends React.Component {
                     <div class="bg-image"></div>
                     <div class="content-wrap">
                         <h1>Sate</h1>
-                        <p>Lorem ipsum dolorss sit amet, consectetur adipisicing elit. Voluptates dolore delectus quia iure possimus laborum expedita
-                            ex inventore ea aliquid.</p>
+                        <p>Your Food Recommdator to help pick out the best spot for your fav meal.</p>
                         <a href="#showcase" class="btn">Register</a>
                     </div>
                 </div>
